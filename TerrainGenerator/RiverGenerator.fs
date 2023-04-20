a41f03eae42db5a43db37735bb51af0c2a0f4719
SuperJaremy <legoitcomp@gmail.com>
2023-04-20 23:50:01 +0300

    River generation

---
+++ TerrainGenerator/RiverGenerator.fs
@@ -0,0 +1,71 @@
+﻿module TerrainGenerator.RiverGenerator
+
+open System
+
+type Direction =
+    | Up
+    | Down
+    | Left
+    | Right
+
+let directionToCoordinate x y direction =
+    match direction with
+    | Up -> (x - 1, y)
+    | Down -> (x + 1, y)
+    | Left -> (x, y - 1)
+    | Right -> (x, y + 1)
+
+let logProbability (mapWidth: int) (mapHeight: int) (velocity: int) =
+    let side = max mapHeight mapWidth
+    ((log ((velocity + 1) |> float)) / (log ((side + 1) |> float))) * 0.15 + 0.7
+
+let rec private step (x, y) probabilityFunc velocity direction (rng: Random) (map: Tiles.TerrainTile[,]) =
+    let mapHeight = Array2D.length1 map
+    let mapWidth = Array2D.length2 map
+
+    if
+        not (
+            x > 0
+            && x < mapHeight
+            && y > 0
+            && y < mapWidth
+            && map.[x, y] <> Tiles.TerrainTile.Water
+        )
+    then
+        map
+    else
+        map.[x, y] <- Tiles.TerrainTile.Water
+
+        let sideOne, sideTwo =
+            match direction with
+            | Up -> Left, Right
+            | Down -> Left, Right
+            | Left -> Up, Down
+            | Right -> Up, Down
+
+        let directProbability = probabilityFunc mapWidth mapHeight velocity
+        let sideProbability = (1.0 - directProbability) / 2.0
+        let rand = rng.NextDouble()
+
+        match rand with
+        | _ when rand < directProbability ->
+            step (directionToCoordinate x y direction) probabilityFunc (velocity + 1) direction rng map
+        | _ when rand < (directProbability + sideProbability) ->
+            step (directionToCoordinate x y sideOne) probabilityFunc 1 sideOne rng map
+        | _ -> step (directionToCoordinate x y sideTwo) probabilityFunc 1 sideTwo rng map
+
+
+let generateRiver x y probabilityFunc seed (map: Tiles.TerrainTile[,]) =
+    let rng = Random(seed)
+    let rand = rng.NextDouble()
+
+    map.[x, y] <- Tiles.TerrainTile.Water
+    
+    let direction =
+        match rand with
+        | _ when rand < 0.25 -> Up
+        | _ when rand < 0.5 -> Down
+        | _ when rand < 0.75 -> Left
+        | _ -> Right
+
+    step (directionToCoordinate x y direction) probabilityFunc 1 direction rng map
--- Visualizer/Program.fs
+++ Visualizer/Program.fs
@@ -3,15 +3,51 @@
 open TerrainGenerator
 open Visualizer
 
-seq {0..10} |> Array.ofSeq |> Array.Parallel.iter (fun x ->
-    let terrain = TerrainGeneration.generateTerrain 9 1024 0.7 0.005f x 102737
-    let res = Drawer.draw terrain 10
-    let path = sprintf "C:\Users\SJar\RiderProjects\TerrainGenerator\ss%d.png" x
-    res.Save(path)
-    )
+let n = 11
+let sideSize = (pown 2 n) + 1
+let variance = 256
+let ratio = 1
+let scale = 0.001f
+let clear = 3
+let seed = 26552
+
+
+// seq {0..10} |> Array.ofSeq |> Array.Parallel.iter (fun x ->
+//     let terrain = TerrainGeneration.generateTerrain 9 1024 0.7 0.005f x 102737
+//     let res = Drawer.draw terrain 10
+//     let path = sprintf "C:\Users\SJar\RiderProjects\TerrainGenerator\ss%d.png" x
+//     res.Save(path)
+//     )
 
 // for i in 0..10 do
 //     let terrain = TerrainGeneration.generateTerrain 10 1024 0.7 0.005f i 52802
 //     let res = Drawer.draw terrain 10
 //     let path = sprintf "C:\Users\SJar\RiderProjects\TerrainGenerator\ss%d.png" i
 //     res.Save(path)
+
+// let terrain = TerrainGeneration.generateTerrain n variance ratio scale clear seed
+//
+// let dsMap =
+//      DiamondSquare.generateMap n 0 0 0 0 variance (seed + 1) DiamondSquare.normalizeBySlide
+//      |> Array2D.map (BiomeGeneration.toTableIndexMapper Tiles.TerrainTable false)
+//      
+// let dsMapRes = Drawer.draw Drawer.debugToColor dsMap 10
+//      
+// let snMap =
+//      SimplexNoise.generateMap sideSize sideSize scale seed
+//      |> Array2D.map (BiomeGeneration.toTableIndexMapper Tiles.TerrainTable true)
+//      
+// let snMapRes = Drawer.draw Drawer.debugToColor snMap 10
+// let res = Drawer.draw Drawer.tileToColor terrain 10
+//
+// dsMapRes.Save("C:\Users\SJar\RiderProjects\TerrainGenerator\ds.png")
+// snMapRes.Save("C:\Users\SJar\RiderProjects\TerrainGenerator\sn.png")
+// res.Save("C:\Users\SJar\RiderProjects\TerrainGenerator\ss.png")
+
+let tiles = Array2D.create 129 129 (Tiles.TerrainTile.Land Tiles.LandTile.Grassland)
+
+let river = RiverGenerator.generateRiver 65 65 RiverGenerator.logProbability seed tiles
+
+let riverRes = Drawer.draw Drawer.tileToColor river 10
+
+riverRes.Save("C:\Users\SJar\RiderProjects\TerrainGenerator\\river.png")
