f3d42953fdf7089e7885fa985d22b3cfadc62ba5
SuperJaremy <legoitcomp@gmail.com>
2023-04-20 23:49:47 +0300

    Generate biomes from with table

--- TerrainGenerator/BiomeGeneration.fs
+++ TerrainGenerator/BiomeGeneration.fs
@@ -10,3 +10,21 @@ let generateBiomes landWaterMap (map: float[,]) =
                 let biomeNum = map.[x, y]
                 Tiles.TerrainTile.Land(Tiles.chooseTile biomeNum))
         landWaterMap
+
+let toTableIndexMapper mapTable isRow toMap =
+    let columns = (List.length mapTable)
+    let rows = (List.length mapTable.[0])
+    if isRow then
+        toMap * (rows |> float) |> floor |> int |> min (rows - 1)
+    else
+        toMap * (columns |> float) |> floor |> int |> min (columns - 1)
+
+let generateBiomesFromTable (elevationMap: float[,]) (moistureMap: float[,]) (mapTable: Tiles.TerrainTile list list) =
+    let indexer = toTableIndexMapper mapTable
+    let elevaTrans = Array2D.map (indexer false) elevationMap
+    let moistureTrans = Array2D.map (indexer true) moistureMap
+    Array2D.mapi (fun x y value ->
+        let elevation = value
+        let moisture = moistureTrans.[x,y]
+        mapTable.[elevation].[moisture]) elevaTrans
+    
--- TerrainGenerator/TerrainGeneration.fs
+++ TerrainGenerator/TerrainGeneration.fs
@@ -4,35 +4,34 @@ open System
 
 let generateTerrain n variance ratio scale clean seed =
     let sideSize = (pown 2 n) + 1
-    let rng = Random(seed)
-    //
+    
     // let dsMap =
     //     DiamondSquare.generateMap n (variance * 2.0) (variance / 2.0) 0.0 variance variance seed DiamondSquare.normalizeByAbs
-    //     |> CellularAutomaton.generateMap clean CellularAutomaton.chooseByAverage
-    // // let dsMap1 =
-    // DiamondSquare.generateMap n 0 0 0 0 variance (seed + 1) DiamondSquare.normalizeBySlide
+        // |> CellularAutomaton.generateMap clean CellularAutomaton.chooseByAverage
+    let dsMap =
+        DiamondSquare.generateMap n 0 0 0 0 variance (seed + 1) DiamondSquare.normalizeBySlide
     // |> CellularAutomaton.step CellularAutomaton.chooseByAverage
     //
-    // let snMap =
-    //     SimplexNoise.generateMap sideSize sideSize scale seed
-    //     |> CellularAutomaton.generateMap clean CellularAutomaton.chooseByAverage
+    let snMap =
+        SimplexNoise.generateMap sideSize sideSize scale seed
+        // |> CellularAutomaton.generateMap clean CellularAutomaton.chooseByAverage
         
-    let whiteNoiseMap = CellularAutomaton.generateWhiteNoise sideSize rng
+    // let whiteNoiseMap = CellularAutomaton.generateWhiteNoise sideSize seed
     
-    let landWaterMap =
-        CellularAutomaton.generateDualWhiteNoise 0.4 sideSize rng
-        |> Array2D.map (fun value ->
-            match value with
-            | 0 -> Tiles.TerrainTile.Water
-            | _ -> Tiles.TerrainTile.Land Tiles.LandTile.Field)
-        |> CellularAutomaton.generateMap clean (CellularAutomaton.chooseByMajority Tiles.TerrainTile.Water)
+    // let landWaterMap =
+    //     CellularAutomaton.generateDualWhiteNoise 0.4 sideSize seed
+    //     |> Array2D.map (fun value ->
+    //         match value with
+    //         | 0 -> Tiles.TerrainTile.Water
+    //         | _ -> Tiles.TerrainTile.Land Tiles.LandTile.Grassland)
+        // |> CellularAutomaton.generateMap clean (CellularAutomaton.chooseByMajority Tiles.TerrainTile.Water)
     // let landWaterMap =
     //     LandWaterSplice.landWater ratio LandWaterSplice.calcFactorMean dsMap
     
-    let biomeMap = BiomeGeneration.generateBiomes landWaterMap whiteNoiseMap
+    // let biomeMap = BiomeGeneration.generateBiomes landWaterMap whiteNoiseMap
     
-    biomeMap
-    |> CellularAutomaton.generateMap clean (CellularAutomaton.chooseByMajority (Tiles.TerrainTile.Land(Tiles.LandTile.Sand)))
+    // biomeMap
+    // |> CellularAutomaton.generateMap clean (CellularAutomaton.chooseByMajority (Tiles.TerrainTile.Land(Tiles.LandTile.Snow)))
 
     // let landWater =
     //     CellularAutomaton.generateWhiteNoise 0.4 129 rng
@@ -48,3 +47,6 @@ let generateTerrain n variance ratio scale clean seed =
 
     // cellular
         // |> LandWaterSplice.landWater 0.6 LandWaterSplice.calcFactorMean
+        
+    BiomeGeneration.generateBiomesFromTable dsMap snMap Tiles.TerrainTable
+        |> CellularAutomaton.generateMap clean (CellularAutomaton.chooseByMajority Tiles.TerrainTile.Water)
