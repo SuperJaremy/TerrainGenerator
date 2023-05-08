8411f5eea33a3ab8741b0a8ba6154e0c23d4ee56
SuperJaremy <legoitcomp@gmail.com>
2023-05-08 16:41:29 +0300

    Add river generation through height map

--- TerrainGenerator/RiverGenerator.fs
+++ TerrainGenerator/RiverGenerator.fs
@@ -55,12 +55,12 @@ let rec private step (x, y) probabilityFunc velocity direction (rng: Random) (ma
         | _ -> step (directionToCoordinate x y sideTwo) probabilityFunc 1 sideTwo rng map
 
 
-let generateRiver x y probabilityFunc seed (map: Tiles.TerrainTile[,]) =
+let probabilityRiverGenerator x y probabilityFunc seed (map: Tiles.TerrainTile[,]) =
     let rng = Random(seed)
     let rand = rng.NextDouble()
 
     map.[x, y] <- Tiles.TerrainTile.Water
-    
+
     let direction =
         match rand with
         | _ when rand < 0.25 -> Up
@@ -69,3 +69,96 @@ let generateRiver x y probabilityFunc seed (map: Tiles.TerrainTile[,]) =
         | _ -> Right
 
     step (directionToCoordinate x y direction) probabilityFunc 1 direction rng map
+
+let rec private heightMapStep x y visited (heightMap: float[,]) flowChooser (map: Tiles.TerrainTile[,]) =
+    let mapWidth = Array2D.length2 map
+    let mapHeight = Array2D.length1 map
+    let left = x, y - 1
+    let right = x, y + 1
+    let up = x - 1, y
+    let down = x + 1, y
+
+    let leftHeight =
+        (Left,
+         (if y > 0 && not (List.contains left visited) then
+              Some map.[fst left, snd left], Some heightMap.[fst left, snd left]
+         else
+              None, None))
+        |> fun (a, (b, c)) -> (a, b, c)
+
+    let rightHeight =
+        (Right,
+         if y < mapWidth - 1 && not (List.contains right visited) then
+             Some map.[fst right, snd right], Some heightMap.[fst right, snd right]
+         else
+             None, None)
+        |> fun (a, (b, c)) -> (a, b, c)
+
+    let upHeight =
+        (Up,
+         (if x > 0 && not (List.contains up visited) then
+              Some map.[fst up, snd up], Some heightMap.[fst up, snd up]
+         else
+              None, None))
+        |> fun (a, (b, c)) -> (a, b, c)
+
+    let downHeight =
+        (Down,
+         if x < mapHeight - 1 && not (List.contains down visited) then
+             Some map.[fst down, snd down], Some heightMap.[fst down, snd down]
+         else
+             None, None)
+        |> fun (a, (b, c)) -> (a, b, c)
+    
+    let res =
+        match (flowChooser leftHeight rightHeight upHeight downHeight) with
+        | Some Left -> Some left
+        | Some Right -> Some right
+        | Some Up -> Some up
+        | Some Down -> Some down
+        | _ -> None
+
+    match res with
+    | None -> map
+    | Some value ->
+        let newX, newY = value
+        map.[newX, newY] <- Tiles.TerrainTile.Water
+        heightMapStep newX newY (value :: visited) heightMap flowChooser map
+
+let heightMapRiverGenerator initX initY (heightMap: float[,]) flowChooser (map: Tiles.TerrainTile[,]) =
+    map.[initX, initY] <- Tiles.TerrainTile.Water
+    heightMapStep initX initY [] heightMap flowChooser map
+
+let maxFlowChooser endCondition left right up down =
+
+    let dir, tile, value =
+        Seq.ofList [ left; right; up; down ]
+        |> Seq.filter (fun (_, _, z) -> Option.isSome z)
+        |> Seq.maxBy (fun (_, _, z) -> Option.get z)
+
+    if not (endCondition (dir, tile, value)) then
+        Some dir
+    else
+        None
+
+let minFlowChooser endCondition left right up down =
+    
+    
+    try
+        let dir, tile, value =
+            Seq.ofList [ left; right; up; down ]
+            |> Seq.filter (fun (_, _, z) -> Option.isSome z)
+            |> Seq.minBy (fun (_, _, z) -> Option.get z)
+
+        if not (endCondition (dir, tile, value)) then
+            Some dir
+        else
+            None
+    with
+    | :? ArgumentException -> None
+
+let isWater (_, tile, _) =
+    Option.get tile = Tiles.TerrainTile.Water
+    
+let isSnow (_, tile, _) =
+    Option.get tile = Tiles.TerrainTile.Land(Tiles.LandTile.Snow)
