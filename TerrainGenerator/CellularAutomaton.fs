5b9f2fe7f797280993a121544bd47b540099579c
SuperJaremy <legoitcomp@gmail.com>
2023-05-08 19:16:25 +0300

    fantomas

--- TerrainGenerator/CellularAutomaton.fs
+++ TerrainGenerator/CellularAutomaton.fs
@@ -1,45 +1,5 @@
 ﻿module TerrainGenerator.CellularAutomaton
 
-open System
-open Microsoft.FSharp.Core
-
-let private excludeCenter (x: seq<'a>) =
-    x |> Seq.except ([ (Seq.item ((Seq.length x) / 2) x) ] |> Seq.ofList)
-
-let chooseByAverage (cell: Option<float>[,]) =
-    cell
-    |> Seq.cast<Option<float>>
-    |> excludeCenter
-    |> Seq.filter Option.isSome
-    |> Seq.map Option.get
-    |> Seq.average
-
-let private majorityFolder state value =
-     let chooser (tag, _) = tag = value
-
-     if List.exists chooser state then
-        let _, cnt = List.find chooser state
-        (List.filter (chooser >> not) state) @ [ (value, (cnt + 1)) ]
-     else
-         state @ [ (value, 1) ]
-
-let chooseByMajority (cell: Option<'a>[,]) =
-    cell
-    |> Seq.cast
-    |> Seq.filter Option.isSome
-    |> Seq.map Option.get
-    |> Seq.fold majorityFolder []
-    |> List.maxBy snd
-    |> fst
-
-let chooseByMajorityWithCorner cornerValue (cell: Option<'a>[,]) =
-    cell
-    |> Seq.cast
-    |> Seq.map (Option.defaultValue cornerValue)
-    |> Seq.fold majorityFolder []
-    |> List.maxBy snd
-    |> fst
-
 let private step (transformer: Option<'a>[,] -> 'a) (map: 'a[,]) =
     let mapHeight = Array2D.length1 map
     let mapWidth = Array2D.length2 map
@@ -57,5 +17,41 @@ let rec generateMap stepCnt transformer initMap =
     if stepCnt = 0 then
         initMap
     else
-        step transformer initMap
-        |> generateMap (stepCnt - 1) transformer
+        step transformer initMap |> generateMap (stepCnt - 1) transformer
+
+let private excludeCenter (x: seq<'a>) =
+    x |> Seq.except ([ (Seq.item ((Seq.length x) / 2) x) ] |> Seq.ofList)
+
+let chooseByAverage (cell: Option<float>[,]) =
+    cell
+    |> Seq.cast<Option<float>>
+    |> excludeCenter
+    |> Seq.filter Option.isSome
+    |> Seq.map Option.get
+    |> Seq.average
+
+let private majorityFolder state value =
+    let chooser (tag, _) = tag = value
+
+    if List.exists chooser state then
+        let _, cnt = List.find chooser state
+        (List.filter (chooser >> not) state) @ [ (value, (cnt + 1)) ]
+    else
+        state @ [ (value, 1) ]
+
+let chooseByMajority (cell: Option<'a>[,]) =
+    cell
+    |> Seq.cast
+    |> Seq.filter Option.isSome
+    |> Seq.map Option.get
+    |> Seq.fold majorityFolder []
+    |> List.maxBy snd
+    |> fst
+
+let chooseByMajorityWithCorner cornerValue (cell: Option<'a>[,]) =
+    cell
+    |> Seq.cast
+    |> Seq.map (Option.defaultValue cornerValue)
+    |> Seq.fold majorityFolder []
+    |> List.maxBy snd
+    |> fst
