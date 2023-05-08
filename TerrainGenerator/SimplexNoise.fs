af5c2084351bb3b44aa7af981e41cf3563501d16
SuperJaremy <legoitcomp@gmail.com>
2023-05-08 17:21:57 +0300

    fantomas

--- TerrainGenerator/SimplexNoise.fs
+++ TerrainGenerator/SimplexNoise.fs
@@ -2,12 +2,12 @@
 
 let generateMap width height scale seed normalizeFunc =
     SimplexNoise.Noise.Seed <- seed
+
     SimplexNoise.Noise.Calc2D(width, height, scale)
     |> Array2D.map float
     |> normalizeFunc
 
 let normalizeByMinMax min max map =
     Array2D.map (fun value -> (value - min) / (max - min)) map
-    
-let normalizeByZeroAndTwoFiftyFive map =
-    normalizeByMinMax 0. 255. map
+
+let normalizeByZeroAndTwoFiftyFive map = normalizeByMinMax 0. 255. map
