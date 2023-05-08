58592cfa5fe26052250fb7fc66138c0593307e89
SuperJaremy <legoitcomp@gmail.com>
2023-05-08 17:24:45 +0300

    Add normalizeBySlide normalizer

--- TerrainGenerator/SimplexNoise.fs
+++ TerrainGenerator/SimplexNoise.fs
@@ -11,3 +11,8 @@ let normalizeByMinMax min max map =
     Array2D.map (fun value -> (value - min) / (max - min)) map
 
 let normalizeByZeroAndTwoFiftyFive map = normalizeByMinMax 0. 255. map
+
+let normalizeBySlide map =
+    let seq1 = map |> Seq.cast<float>
+    let seqMin, seqMax = Seq.min seq1, Seq.max seq1
+    normalizeByMinMax seqMin seqMax map
