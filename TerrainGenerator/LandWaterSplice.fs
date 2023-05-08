52eb98aadfb61ac77292edac9ea91213f8fae946
SuperJaremy <legoitcomp@gmail.com>
2023-05-08 18:51:33 +0300

    fantomas

--- TerrainGenerator/LandWaterSplice.fs
+++ TerrainGenerator/LandWaterSplice.fs
@@ -1,15 +1,5 @@
 ﻿module TerrainGenerator.LandWaterSplice
 
-let calcFactorMean (ratio: float) map =
-    let mean = map |> Seq.cast |> Seq.average
-    mean * ratio
-
-let calcFactorMed (ratio: float) map =
-    let seq1 = map |> Seq.cast
-    let sorted = Seq.sort<float> seq1
-    let med = Seq.item ((Seq.length sorted) / 2) sorted |> float
-    med * ratio
-
 let landWater (ratio: float) factorCalculator map =
     let factor = factorCalculator ratio map
 
@@ -19,3 +9,13 @@ let landWater (ratio: float) factorCalculator map =
             Tiles.TerrainTile.Water
         else
             Tiles.TerrainTile.Land Tiles.LandTile.Grassland)
+
+let calcFactorMean (ratio: float) map =
+    let mean = map |> Seq.cast |> Seq.average
+    mean * ratio
+
+let calcFactorMed (ratio: float) map =
+    let seq1 = map |> Seq.cast
+    let sorted = Seq.sort<float> seq1
+    let med = Seq.item ((Seq.length sorted) / 2) sorted |> float
+    med * ratio
